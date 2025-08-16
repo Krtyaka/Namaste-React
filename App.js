13d02dfcb8d80f80be4d2d55964ca746effc87c4
Krtyaka <krtyaka.ahuja@gmail.com>
2025-08-16 20:36:10 +0530

    configured npm and learnt about parcel

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 //the second parameter that is the object is for giving attributes to the element that we are creating
 const heading = React.createElement("h1", {id: 'heading' }, "Hello World from React!");
 //here, heading is a React Element (meaning a javascript object and not the h1 tag itself)
 
 //creating nested elements
-const parent = React.createElement("div", {id: "parent"},
+const parent = React.createElement("div", {id: "parent"}, [
     React.createElement("div", {id: "child"}, [
         //array of children for creating siblings
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag")
+        React.createElement("h1", {key: "div1-h1"}, "I am an h1 tag"),
+        React.createElement("h2", {key: "div1-h2"}, "I am an h2 tag")
+        ]),
+    React.createElement("div", {id: "child"}, [
+        //array of children for creating siblings
+        React.createElement("h1", {key: "div2-h1"}, "I am an h1 tag"),
+        React.createElement("h2", {key: "div2-h2"}, "I am an h2 tag")
         ])
+    ]
 );
 
 console.log(parent);   //object
