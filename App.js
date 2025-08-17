00d56323b1abbe2e8c2ff2959071c86996a2c051
Krtyaka <krtyaka.ahuja@gmail.com>
2025-08-17 22:05:00 +0530

    lesson-3 learning  about JSX and components

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-//the second parameter that is the object is for giving attributes to the element that we are creating
-const heading = React.createElement("h1", {id: 'heading' }, "Hello World from React!");
-//here, heading is a React Element (meaning a javascript object and not the h1 tag itself)
+const Title = () => <h1> Namaste React using JSX </h1>;
 
-//creating nested elements
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        //array of children for creating siblings
-        React.createElement("h1", {key: "div1-h1"}, "I am an h1 tag"),
-        React.createElement("h2", {key: "div1-h2"}, "I am an h2 tag")
-        ]),
-    React.createElement("div", {id: "child"}, [
-        //array of children for creating siblings
-        React.createElement("h1", {key: "div2-h1"}, "I am an h1 tag"),
-        React.createElement("h2", {key: "div2-h2"}, "I am an h2 tag")
-        ])
-    ]
+// React Functional Component
+// Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading"> Namaste React Functional Component </h1>
+  </div>
 );
-
-console.log(parent);   //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);   //the render function converts the javascript object/react element into the heading tag
+// REPLACES any existing code in the "root" element does not append.
+root.render(<HeadingComponent />);
